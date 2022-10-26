bf44052be16656d9fbe5c8cb9a62a67fac393b52
Mikulew <mikulew@gmail.com>
2022-10-26 16:37:10 +0200

    Add styles

--- utils/dom.js
+++ utils/dom.js
@@ -1,8 +1,10 @@
 const createFlagImageElement = (url) => {
     const imageContainerElement = document.createElement('div');
     const imageElement = document.createElement('img');
+
     imageElement.src = url;
     imageContainerElement.appendChild(imageElement)
+
     return imageContainerElement;
 };
 
@@ -10,28 +12,40 @@ const createDescriptionElement = (label, value) => {
     const descriptionElement = document.createElement('div');
     const labelDescription = document.createElement('strong');
     const valueDescription = document.createElement('span');
-    labelDescription.innerText = `${label}:`;
+
+    labelDescription.innerText = `${label}: `;
     valueDescription.innerText = value;
+
     descriptionElement.appendChild(labelDescription);
     descriptionElement.appendChild(valueDescription);
+
     return descriptionElement;
 };
 
 const createCountryElement = (country) => {
     const countryElement = document.createElement('li');
-    const countryName = document.createElement('span');
+    const countryContainer = document.createElement('div');
+    const countryName = document.createElement('strong');
+
+    countryElement.classList.add('country-container');
+    countryContainer.classList.add('country-info-container');
+    countryName.classList.add('country-name');
     countryName.innerText = country.name;
+
     countryElement.appendChild(createFlagImageElement(country.flagUrl));
-    countryElement.appendChild(countryName);
-    countryElement.appendChild(createDescriptionElement("Population", country.population));
-    countryElement.appendChild(createDescriptionElement("Region", country.region));
-    countryElement.appendChild(createDescriptionElement("Capital", country.capital));
+    countryContainer.appendChild(countryName);
+    countryContainer.appendChild(createDescriptionElement("Population", country.population));
+    countryContainer.appendChild(createDescriptionElement("Region", country.region));
+    countryContainer.appendChild(createDescriptionElement("Capital", country.capital));
+    countryElement.appendChild(countryContainer);
+
     return countryElement;
 };
 
 const createListElement = (countries) => {
     const listElement = document.createElement('ul');
     countries.forEach(country => listElement.appendChild(createCountryElement(country)));
+
     return listElement;
 };
 
